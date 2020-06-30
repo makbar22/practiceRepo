b03b1a237704cf9fe83eadde7e6c895bb5bc4c90
Murtaza Akbar <makbar22>
2020-06-30 19:23:32 -0400

    practic git

---
+++ src/main/java/GITprojectPractice/GITprojectPr/App.java
@@ -0,0 +1,13 @@
+package GITprojectPractice.GITprojectPr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
